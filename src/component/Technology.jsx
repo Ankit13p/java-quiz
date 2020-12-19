b777d413878754647a2a083d9dacf09c91a71709
Ankitkumar Prajapati <42886562+Ankit13p@users.noreply.github.com>
2020-12-19 22:19:01 +1300

    Update Technology.jsx

--- src/component/Technology.jsx
+++ src/component/Technology.jsx
@@ -11,8 +11,10 @@ class Technology extends Component{
     }
     render(){
     return(
-        <div>
         <div className="container mt-5 mb-2 p-3" >
+         <div className="">
+                <button className="btn draw-border animate__animated animate__fadeInUp" onClick={this.onClickHandle}>Start quiz</button>
+        </div>
             <div class="panel panel-default">
                 <div class="panel-body">
                     <div class="container">
@@ -55,13 +57,7 @@ class Technology extends Component{
                     </div>
                 </div>
             </div>
-            
         </div>
-         <div className="">
-                <button className="btn draw-border animate__animated animate__fadeInUp" onClick={this.onClickHandle}>Start quiz</button>
-        </div>
-        </div>
-       
     );
     }
 }
