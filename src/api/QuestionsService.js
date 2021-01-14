e34452ecbd15df171ef3f4e8609b4961f75aa9a2
Ankitkumar Prajapati <42886562+Ankit13p@users.noreply.github.com>
2021-01-14 15:27:11 +1300

    Backend hosted

--- src/api/QuestionsService.js
+++ src/api/QuestionsService.js
@@ -2,17 +2,17 @@ import axios from 'axios'
 
 class QuestionsService{
     retriveAllQuestions(){
-        return  axios.get(`http://localhost:8080/rest/questions/all`);
+        return  axios.get(`http://3.130.171.62:9090/rest/questions/all`);
     }
 
     retriveQuestion(id){
-        return  axios.get(`http://localhost:8080/rest/questions/${id}`);
+        return  axios.get(`http://3.130.171.62:9090/rest/questions/${id}`);
     }
     createQuestion(question){
-        return  axios.post(`http://localhost:8080/rest/questions/load`, question);
+        return  axios.post(`http://3.130.171.62:9090/rest/questions/load`, question);
     }
     updateQuestion(id, question){
-        return  axios.put(`http://localhost:8080/rest/questions/load/${id}`, question);
+        return  axios.put(`http://3.130.171.62:9090/rest/questions/load/${id}`, question);
     }
 }
 export default new QuestionsService();
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -7,7 +7,7 @@ class Footer extends Component{
             <div className="mt-5">
              <footer className="footer bg-dark text-center pt-2 pb-2 fixed-bottom">
                 <span className="text-light">Created by Ankit Prajapati</span>
-            </footer>
+            </footer> 
             </div>
         );
     }
