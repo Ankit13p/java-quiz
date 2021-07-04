37e6fe65564e285d8cef83122a89c9a2ee802fe1
Ankitkumar Prajapati <42886562+Ankit13p@users.noreply.github.com>
2021-07-04 14:44:14 +1200

    Local host try

--- src/api/QuestionsService.js
+++ src/api/QuestionsService.js
@@ -7,13 +7,13 @@ class QuestionsService{
     }
 
     retriveQuestion(id){
-        return  axios.get(`http://3.130.171.62:9090/rest/questions/${id}`);
+        return  axios.get(`http://localhost:8080/rest/questions/${id}`);
     }
     createQuestion(question){
-        return  axios.post(`http://3.130.171.62:9090/rest/questions/load`, question);
+        return  axios.post(`http://localhost:8080/rest/questions/load`, question);
     }
     updateQuestion(id, question){
-        return  axios.put(`http://3.130.171.62:9090/rest/questions/load/${id}`, question);
+        return  axios.put(`http://localhost:8080/rest/questions/load/${id}`, question);
     }
 }
 export default new QuestionsService();
